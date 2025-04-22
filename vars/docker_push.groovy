dee5cfd1435cdb0d9291d14d4fe57b2816b51052
siddevops35 <135475200+siddevops35@users.noreply.github.com>
2025-04-22 14:05:50 -0400

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String ProjectName,String ImageTag,String dockerHubUser){
+def call(String ProjectName,String ImageTag,String dockerhubuser){
   withCredentials([userPassword('credentialsId:"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
-    sh "docker login -u ${dockerHubUser} -p {dockerHubPass} "
+    sh "docker login -u ${dockerhubuser} -p {dockerhubpass} "
   }
-  sh "docker push $(dockerHubUser)/${ProjectName}:${ImageTag} "
+  sh "docker push $(dockerhubuser)/${ProjectName}:${ImageTag} "
 }
